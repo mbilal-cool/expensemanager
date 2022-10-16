7b495117bf3db0f1e984cb26d662b176a8546c4b
UmarLE <umar@launchave.io>
2022-10-16 11:09:26 +0500

    icon and boot splash added

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {store} from '../expensemanager/src/utils/Redux/store';
 import MainNavigation from './src/Navigation/mainNavigation';
-
+import RNBootSplash from 'react-native-bootsplash';
 // RNBootSplash.hide(); // immediate
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
   return (
     <Provider store={store}>
       <MainNavigation />
--- android/app/src/main/java/com/expensemanager/MainActivity.java
+++ android/app/src/main/java/com/expensemanager/MainActivity.java
@@ -3,20 +3,31 @@ package com.expensemanager;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this); // <- initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+  }
+
   @Override
   protected String getMainComponentName() {
     return "expensemanager";
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
    */
   @Override
